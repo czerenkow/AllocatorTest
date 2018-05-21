ee6db9edb4f7a3f252cf0aef563e687090c4f1bf
Piotr Woloszkiewicz <piotr.woloszkiewicz@gmail.com>
2018-05-21 22:05:36 +0200

    Inital commit

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include <set>
+#include <map>
+#include <random>
+#include <memory>
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+#include <cstdlib>
+#include <algorithm>
+
+
+int main()
+{
+    std::cout << "ala ma kota\n";
+    return 0;
+}
